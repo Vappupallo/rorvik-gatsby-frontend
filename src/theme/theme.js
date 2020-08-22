767e1ee085fc540e3a3d037dce6555a1a457a872
Maria Aspiala <mariaaspiala@Marias-MacBook-Pro.local>
2020-08-22 20:17:12 +0300

    WIP colors

---
+++ src/theme/theme.js
@@ -0,0 +1,24 @@
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;600&family=Open+Sans:wght@400;600&display=swap');
+</style>
+
+const theme = {
+    color: {
+        brandBlue: "#0024AA",
+        illustrationBlue: "#001666",
+        background: "F7F7FA",
+        border: "D9DEE9",
+        text: "#333333",
+        secondaryText: "4D546D",
+        white: "#FFFFFF",
+    },
+    fonts: {
+        h1: '"Josefin Sans, sans-serif;", font-weight: 600',
+    },
+    typography: {
+        size: {
+            h1Size: "54px",
+            h2Size: "44px",
+        }
+    }
+}
