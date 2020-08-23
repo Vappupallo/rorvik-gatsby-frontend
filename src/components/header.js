d55bdbb16a59a870f757468137959856d91f99cc
revgains <roger.rajala@gmail.com>
2020-08-23 13:56:00 +0300

    add styled components and global fonts

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,20 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+
+const HeaderWrapper = styled.header`
+  background: blue;
+  margin-bottom: 1.45rem;
+`
+
+const HeaderLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <HeaderWrapper>
     <div
       style={{
         margin: `0 auto`,
@@ -16,19 +22,12 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+      <h1>Rörvik Resort</h1>
+      <h1>
+        <HeaderLink to="/">{siteTitle}</HeaderLink>
       </h1>
     </div>
-  </header>
+  </HeaderWrapper>
 )
 
 Header.propTypes = {
--- src/components/seo.js
+++ src/components/seo.js
@@ -68,7 +68,12 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <link
+        href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Open+Sans&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
   )
 }
 
