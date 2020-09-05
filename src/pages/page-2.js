474b7a3f1a58fa4033d8a1a1873ec2f7ce465417
Maria Aspiala <mariaaspiala@Marias-MacBook-Pro.local>
2020-09-05 11:59:59 +0300

    fixed components name to english and removed wave component

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Ingressi = styled.p`
+const Lead = styled.p`
   font-size: 18px;
 `
 
@@ -17,42 +17,20 @@ const Label = styled.p`
   margin-bottom: 0px;
 `
 
-const WaveDivider = styled.hr`
-  .wave {
-  animation: moveTheWave 2400ms linear infinite;
-  stroke-dasharray: 0 16 101 16;
-}
-
-@keyframes moveTheWave {
-
-  0% {
-    stroke-dashoffset: 0;
-    transform: translate3d(0, 0, 0);
-  }
-
-  100% {
-    stroke-dashoffset: -133;
-    transform: translate3d(-90px, 0, 0);
-  }
-
-}
-`
-
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Rörvikin tyylit</h1>
-    <WaveDivider></WaveDivider>
-    <Ingressi>Käytä näitä tyylejä</Ingressi>
+    <Lead>Käytä näitä tyylejä</Lead>
     <h1>Heading 1</h1>
     <Label>8 cottages</Label>
     <h2>Heading 2</h2>
     <h3>Heading 3</h3>
     <h4>Heading 4</h4>
     <p>Paragraph teksti näyttää tältä</p>
-    <Ingressi>
+    <Lead>
       Ingressi on tyylitelty Paragraph teksti
-    </Ingressi>
+    </Lead>
 
     <Link to="/">Go back to the homepage</Link>
 
